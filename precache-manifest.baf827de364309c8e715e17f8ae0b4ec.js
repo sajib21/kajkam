1b0b88a33a3ad674b5ad833bf9d8744441eaecea
sajib21 <shariarsajib@gmail.com>
2020-05-14 21:22:33 +0600

    Updates

---
+++ precache-manifest.baf827de364309c8e715e17f8ae0b4ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6040e3682c475d59c1719e31f7ac32",
+    "url": "/kajkam/index.html"
+  },
+  {
+    "revision": "c2f20b87829a248df2c0",
+    "url": "/kajkam/static/js/2.24c753b7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kajkam/static/js/2.24c753b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584e59b7a1befa07107a",
+    "url": "/kajkam/static/js/main.4639d197.chunk.js"
+  },
+  {
+    "revision": "cfc3cc178a7982f94c1a",
+    "url": "/kajkam/static/js/runtime-main.cf460f66.js"
+  }
+]);
