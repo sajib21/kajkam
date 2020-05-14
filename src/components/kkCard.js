3c4c6a12df2f5c1d182b57d85cc7802459c08c46
sajib21 <shariarsajib@gmail.com>
2020-05-14 17:51:45 +0600

    Used KKForm and added states in KKCard

--- src/components/kkCard.js
+++ src/components/kkCard.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import { Card, CardContent } from "@material-ui/core";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
+import KKForm from "./kkForm";
 
 const CardContainer = styled.div`
   margin-bottom: 8px;
 `;
 
 const KKCard = ({ text, id, index }) => {
+  const [editMode, setEditMode] = useState(false);
+  const [cardText, setText] = useState(text);
+
+  const closeForm = (e) => {
+    setEditMode(false);
+  };
+
+  const saveCard = () => {};
+
+  if (editMode)
+    return (
+      <KKForm
+        text={cardText}
+        setText={setText}
+        closeForm={closeForm}
+        onAction={saveCard}
+      />
+    );
+
   return (
     <Draggable draggableId={String(id)} index={index}>
       {(provided) => (
