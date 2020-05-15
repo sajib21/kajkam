e965612894129562f58c25d13d898097e086978c
sajib21 <shariarsajib@gmail.com>
2020-05-16 03:13:39 +0600

    Issue Fixed

--- src/components/kkForm.js
+++ src/components/kkForm.js
@@ -3,41 +3,41 @@ import styled from "styled-components";
 import { Card, Button, Icon } from "@material-ui/core";
 import Textarea from "react-textarea-autosize";
 
+const Container = styled.div`
+  width: 284px;
+  margin-bottom: 8px;
+`;
+
+const StyledCard = styled(Card)`
+  min-height: 85px;
+  padding: 6px 8px 2px;
+`;
+
+const StyledTextarea = styled(Textarea)`
+  resize: none;
+  width: 100%;
+  overflow: hidden;
+  outline: none;
+  border: none;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 8px;
+  display: flex;
+  align-items: center;
+  margin-left: 8px;
+`;
+
+const StyledIcon = styled(Icon)`
+  margin-left: 8px;
+  cursor: pointer;
+`;
+
 const KKForm = React.memo(
   ({ list, text = "", onChange, closeForm, children }) => {
     const placeholder = list
       ? "Enter list title..."
       : "Enter a title for this card...";
-
-    const Container = styled.div`
-      width: ${list ? "300px" : "100%"};
-      margin-bottom: 8px;
-    `;
-
-    const StyledCard = styled(Card)`
-      min-height: 85px;
-      padding: 6px 8px 2px;
-    `;
-
-    const StyledTextarea = styled(Textarea)`
-      resize: none;
-      width: 100%;
-      overflow: hidden;
-      outline: none;
-      border: none;
-    `;
-
-    const ButtonContainer = styled.div`
-      margin-top: 8px;
-      display: flex;
-      align-items: center;
-      margin-left: 8px;
-    `;
-
-    const StyledIcon = styled(Icon)`
-      margin-left: 8px;
-      cursor: pointer;
-    `;
 
     return (
       <Container>
