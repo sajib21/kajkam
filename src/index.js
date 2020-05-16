e189361f65ec3aca6e1771f21ec096682fbc3b46
sajib21 <shariarsajib@gmail.com>
2020-05-16 17:37:22 +0600

    Used redux-persist to store data in local storage of browser

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import store from "./store";
+import Store from "./store";
 import { createGlobalStyle } from "styled-components";
 import "./index.css";
 import $ from "jquery";
+import { PersistGate } from "redux-persist/integration/react";
+
+const { persistor, store } = Store();
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -17,8 +20,10 @@ const GlobalStyle = createGlobalStyle`
 
 ReactDOM.render(
   <Provider store={store}>
-    <GlobalStyle />
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <GlobalStyle />
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,17 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(persistedReducer);
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
