6a710f95024266d22a09d2e884121bfa041c3ff7
sajib21 <shariarsajib@gmail.com>
2020-05-21 20:34:44 +0600

    Completed all reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "redux";
+import boardReducer from "./boardReducer";
+import activeBoardReducer from "./activeBoardReducer";
 import listReducer from "./listReducer";
+import cardReducer from "./cardReducer";
 
 export default combineReducers({
+  boards: boardReducer,
+  activeBoard: activeBoardReducer,
   lists: listReducer,
+  cards: cardReducer,
 });
